2e7d5916e02d9c2cdebd090694d0235ba9ca8384
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2021-09-27 15:25:13 -0300

    fix docker tag

--- docker_build.sh
+++ docker_build.sh
@@ -4,7 +4,7 @@ if [ "$#" != "2" ]; then
     exit 1
 else
     docker_tag="${1/v/}"
-    docker_tag_2="$1-$2"    
+    docker_tag_2="${docker_tag}-$2"    
     git tag "${docker_tag_2}" || exit 1
     git push --tags
 fi
