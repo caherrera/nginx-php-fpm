2950e96e8ac79110950625b047080631d38b188c
Carlos Herrera <18551113+caherrera@users.noreply.github.com>
2021-09-27 15:20:41 -0300

    fix docker tag

--- docker_build.sh
+++ docker_build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker build -t carlositline/nginx-php-fpm .
-docker push carlositline/nginx-php-fpm
-
-if [ "$1" == "" ]; then
-    docker_tag=$(git describe --tags --abbrev=0)
+if [ "$#" != "2" ]; then
+    exit 1
 else
-    docker_tag="$1"
-    docker_tag_2="$1-$2"
-    git tag "${docker_tag_2}"
+    docker_tag="${1/v/}"
+    docker_tag_2="$1-$2"    
+    git tag "${docker_tag_2}" || exit 1
     git push --tags
 fi
+
+docker build -t carlositline/nginx-php-fpm .
+docker push carlositline/nginx-php-fpm
 
 docker tag carlositline/nginx-php-fpm:latest "carlositline/nginx-php-fpm:${docker_tag}"
 docker tag carlositline/nginx-php-fpm:latest "carlositline/nginx-php-fpm:${docker_tag_2}"
